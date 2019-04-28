1bfdcc934b6e1b4a7c82a0751e77e9f323b82726
Sirmer Imre <sirmer.imre@gmail.com>
2019-04-28 11:28:08 +0100

    Updates

---
+++ precache-manifest.d9797fc5a2c8f353eb4b6a9fd76b5a68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e0a9fc80b32ec3a05cfdf2af557d83",
+    "url": "/youtubesearch/index.html"
+  },
+  {
+    "revision": "1d4bad377b151f21587d",
+    "url": "/youtubesearch/static/css/main.18b90eab.chunk.css"
+  },
+  {
+    "revision": "cef4b827ae2db54466b4",
+    "url": "/youtubesearch/static/js/2.8b33037e.chunk.js"
+  },
+  {
+    "revision": "1d4bad377b151f21587d",
+    "url": "/youtubesearch/static/js/main.12cb028e.chunk.js"
+  },
+  {
+    "revision": "c085d7f893bba053822b",
+    "url": "/youtubesearch/static/js/runtime~main.7426d7ab.js"
+  }
+]);
